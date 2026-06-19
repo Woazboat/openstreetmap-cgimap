8c700cbd71bedbf38f3922946a8a582150197e6e
Florian Kargl <f.kargl@posteo.de>
2026-06-20 00:23:42 +0200

    Add custom libpqxx 8 to_buf string conversion function for ranges

--- include/cgimap/backend/apidb/pqxx_string_traits.hpp
+++ include/cgimap/backend/apidb/pqxx_string_traits.hpp
@@ -11,9 +11,11 @@
 #define BACKEND_APIDB_PQXX_STRING_TRAITS_HPP
 
 #include <algorithm>
+#include <iterator>
 #include <set>
-#include <sstream>
+#include <string_view>
 #include <vector>
+#include <ranges>
 
 #include <fmt/core.h>
 #include <fmt/format.h>
@@ -58,15 +60,77 @@ namespace cgimap {
 
 // !! cgimap::array_string_traits expects any strings to be already escaped !!
 
-template<typename Container>
+// libpqxx 8 provides built-in string serialization functions for ranges, so
+// providing our own here is technically no longer necessary. However, using the
+// built-in libpqxx functions currently leads to string values being quoted
+// twice, since the existing cgimap range string serialization functions
+// expect strings to be pre-escaped, while the built-in libpqxx functions
+// perform quoting internally.
+//
+// libpqxx v8.0.1 also still has a bug in the built-in serialization
+// functions for ranges of numbers, which means they cannot be used safely
+// (https://github.com/jtv/libpqxx/issues/1235)
+
+template<std::ranges::range Container>
 struct array_string_traits
 {
 private:
-  using elt_type = strip_t<value_type<Container>>;
+  using elt_type = std::remove_cvref_t<value_type<Container>>;
   using elt_traits = string_traits<elt_type>;
 
 public:
   static zview to_buf(char *begin, char *end, Container const &value) = delete;
+
+#if PQXX_VERSION_MAJOR >= 8
+  [[nodiscard]] static inline std::string_view
+  to_buf(std::span<char> buf, Container const &value, ctx c = {})
+  {
+    auto const budget{ size_buffer(value) };
+
+    if (std::cmp_less(std::size(buf), budget))
+      throw pqxx::conversion_overrun{
+        "Not enough buffer space to convert array to string.", c.loc
+      };
+
+    std::size_t here{ 0u };
+    buf[here++] = '{';
+
+    bool nonempty{ false };
+    for (auto const &elt : value)
+    {
+      // This serialization function doesn't work for nullable types
+      static_assert(!pqxx::nullness<elt_type>::has_null);
+
+      auto elt_buf = buf.subspan(here);
+      auto out = elt_traits::to_buf(elt_buf, elt, c);
+      auto sz = std::size(out);
+      auto available_len = std::size(elt_buf);
+
+      if (std::cmp_greater(sz, available_len))
+        throw pqxx::conversion_overrun{
+          std::format(
+              "Buffer too small to convert {} value to string (needs a {}-byte "
+              "buffer).",
+              name_type<elt_type>(), sz),
+          c.loc
+        };
+      // no-op if out.data() == buf.data() + here
+      std::memmove(buf.data() + here, out.data(), sz);
+      here += sz;
+
+      buf[here++] = array_separator<elt_type>;
+      nonempty = true;
+    }
+
+    // Erase that last comma, if present.
+    if (nonempty)
+      here--;
+
+    buf[here++] = '}';
+
+    return { std::data(buf), here };
+  }
+#endif
 
   static char *into_buf(char *begin, char *end, Container const &value)
   {
@@ -98,6 +162,7 @@ public:
 
   static std::size_t size_buffer(Container const &value) noexcept
   {
+      // Assume that no escaping is required (strings already need to be pre-escaped)!
       return 3 + std::accumulate(    // +3 for curly braces and null byte at the end
                    std::begin(value), std::end(value), std::size_t{},
                    [](std::size_t acc, elt_type const &elt) {
@@ -109,6 +174,15 @@ public:
 
 }
 
+// Provide a definition of the pqxx::name_type() function template for backward
+// compatibility if it's missing
+#if PQXX_VERSION_MAJOR < 8
+template<typename T> inline constexpr std::string_view name_type() noexcept
+{
+  return type_name<T>;
+}
+#endif
+
 #define PQXX_ARRAY_STRING_TRAITS(type)                                  \
   template <>                                                           \
   struct nullness<type> : no_null<type> {};                             \
@@ -117,6 +191,8 @@ public:
   struct string_traits<type> : cgimap::array_string_traits<type> {};    \
                                                                         \
   template<> inline std::string const type_name<type>{#type};           \
+  template<> inline constexpr std::string_view                          \
+  name_type<type>() noexcept { return #type; };
 
 
 #endif
